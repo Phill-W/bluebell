381966dfbd2ffe1bb2a5a8c461c34f80f8a8ba1e
Phil <1453139442@qq.com>
2026-07-03 10:53:59 +0800

    feat: first commit for initialize

---
+++ controller/user.go
@@ -0,0 +1,50 @@
+package controller
+
+import (
+	"bluebell/logic"
+	"bluebell/models"
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
+	"go.uber.org/zap"
+)
+
+// SignUpHandler 处理注册请求的函数
+func SignUpHandler(c *gin.Context) {
+	//1. 获取参数与参数校验
+	p := new(models.ParamSignUp)
+	if err := c.ShouldBind(p); err != nil {
+		//请求参数有误，直接返回响应
+		zap.L().Error("SignUP with invalid param", zap.Error(err))
+		//判断err是不是validator.ValidationErrors 类型
+		errs, ok := err.(validator.ValidationErrors)
+		if !ok {
+			c.JSON(http.StatusOK, gin.H{
+				"msg": err.Error(),
+			})
+			return
+		}
+		c.JSON(http.StatusOK, gin.H{
+			"msg": removeTopStruct(errs.Translate(trans)),
+		})
+		return
+	}
+	// 手动对请求参数进行详细的业务规则校验
+	//if len(p.Username) == 0 || len(p.Password) == 0 || len(p.RePassword) == 0 || p.RePassword != p.Password {
+	//	zap.L().Error("SignUP with invalid param")
+	//	c.JSON(http.StatusOK, gin.H{
+	//		"msg": "请求参数错误",
+	//	})
+	//	return
+	//}
+	fmt.Println(p)
+	//2. 业务处理
+	logic.SignUp(p)
+
+	//3. 返回相应
+	c.JSON(http.StatusOK, gin.H{
+		"msg": "success",
+	})
+}
---
+++ dao/mysql/user.go
@@ -0,0 +1,14 @@
+package mysql
+
+/*
+*把每一步数据库操作封装为函数
+*待logic层根据业务需求调用
+ */
+
+func QueryUserByUsername() {
+
+}
+
+func InsertUser() {
+	//执行sql语句入库
+}
