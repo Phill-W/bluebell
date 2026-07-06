f21edebcf5a1a2aa7246fe21d1e77ecde62ced1c
Phil <1453139442@qq.com>
2026-07-06 11:23:57 +0800

    feat: 优化拼接获取帖子详情

--- logic/post.go
+++ logic/post.go
@@ -4,6 +4,8 @@ import (
 	"bluebell/dao/mysql"
 	"bluebell/models"
 	"bluebell/pkg/snowflake"
+
+	"go.uber.org/zap"
 )
 
 // CreatePost 创建帖子
@@ -16,6 +18,29 @@ func CreatePost(p *models.Post) (err error) {
 }
 
 // GetPostByID 获取帖子详情
-func GetPostByID(pid int64) (post *models.Post, err error) {
-	return mysql.GetPostByID(pid)
+func GetPostByID(pid int64) (data *models.ApiPostDetail, err error) {
+	//查询数据并且拼接组合我们接口想要的数据
+	post, err := mysql.GetPostByID(pid)
+	if err != nil {
+		zap.L().Error("mysql.GetPostByID(pid) failed", zap.Int64("pid", pid), zap.Error(err))
+	}
+	//根据作者id查询作者信息
+	user, err := mysql.GetUserByID(post.AuthorID)
+	if err != nil {
+		zap.L().Error("mysql.GetUserInfoByID(post.AuthorID) failed", zap.Int64("author_id", post.AuthorID), zap.Error(err))
+		return
+	}
+	//根据社区id查询社区详细信息
+	community, err := mysql.GetCommunityDetailByID(post.CommunityID)
+	if err != nil {
+		zap.L().Error("mysql.GetCommunityDetailByID(post.CommunityID) failed", zap.Int64("community_id", post.CommunityID), zap.Error(err))
+		return
+	}
+	//将查询到的数据拼接
+	data = &models.ApiPostDetail{
+		AuthorName:      user.Username,
+		CommunityDetail: community,
+		Post:            post,
+	}
+	return
 }
